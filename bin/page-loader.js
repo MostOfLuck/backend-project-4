b23a3df2a034de671cf06272abe5cdc668efdd1a
Sam <burtman.samuel26@gmail.com>
2024-01-20 22:26:27 +0200

    update bin/page-loader.js

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -6,10 +6,13 @@ program
   .name('page-loader')
   .description('Page loader utility')
   .version('1.0.0')
+  .argument('<url>', 'URL of the page to download')
   .option('-o, --output [dir]', 'output directory', process.cwd())
   .action((url, options) => {
     downloadPage(url, options.output)
-      .then(filePath => console.log(`Page was downloaded into '${filePath}'`))
+      .then(filePath => {
+        console.log(filePath);
+      })
       .catch(error => console.error(`Error: ${error.message}`));
   });
 
