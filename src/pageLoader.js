af358f17759c68eb723cafb99765c7e98043f0da
Sam <burtman.samuel26@gmail.com>
2024-01-20 22:26:40 +0200

    update src/pageLoader.js

--- src/pageLoader.js
+++ src/pageLoader.js
@@ -1,7 +1,13 @@
-#!/usr/bin/env node
 import axios from 'axios';
 import { promises as fs } from 'fs';
 import path from 'path';
+
+const urlToFileName = (url) => {
+  const myURL = new URL(url);
+  const hostname = myURL.hostname;
+  const pathname = myURL.pathname.replace(/\/$/, '');
+  return `${hostname}${pathname}`.replace(/[^a-zA-Z0-9]/g, '-') + '.html';
+};
 
 const downloadPage = (url, outputDir) => {
   const fileName = urlToFileName(url);
@@ -10,13 +16,6 @@ const downloadPage = (url, outputDir) => {
   return axios.get(url)
     .then(response => fs.writeFile(filePath, response.data))
     .then(() => filePath);
-};
-
-const urlToFileName = (url) => {
-  if (typeof url !== 'string') {
-    throw new Error('URL must be a string');
-  }
-  return url.replace(/https?:\/\//, '').replace(/[^a-zA-Z0-9]/g, '-') + '.html';
 };
 
 export default downloadPage;
