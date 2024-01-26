f22af6898b51291c19e856b8a7422ed892325a28
Sam <burtman.samuel26@gmail.com>
2024-01-26 18:21:17 +0200

    add logic to download pages

--- src/pageLoader.js
+++ src/pageLoader.js
@@ -3,39 +3,62 @@ import axios from 'axios';
 import cheerio from 'cheerio';
 import { promises as fs } from 'fs';
 import path from 'path';
+import https from 'https';
+
+const httpsAgent = new https.Agent({  
+  rejectUnauthorized: false
+});
+
+const urlToFileName = (url, defaultExtension = '') => {
+  const { hostname, pathname } = new URL(url);
+  const extension = path.extname(pathname) || defaultExtension;
+  return `${hostname}${pathname.replace(extension, '')}`.replace(/[^a-zA-Z0-9]/g, '-') + extension;
+};
+
+const urlToDirectoryName = (url) => {
+  const { hostname, pathname } = new URL(url);
+  return `${hostname}${pathname.replace(/\/$/, '')}`.replace(/[^a-zA-Z0-9]/g, '-') + '_files';
+};
+
+const downloadResource = async (url, outputPath) => {
+  const response = await axios.get(url, { 
+    responseType: 'arraybuffer',
+    httpsAgent: url.startsWith('https') ? httpsAgent : undefined
+  });
+  await fs.writeFile(outputPath, response.data);
+};
 
 const downloadPage = async (url, outputDir) => {
-  const response = await axios.get(url);
+  const response = await axios.get(url, {
+    httpsAgent: url.startsWith('https') ? httpsAgent : undefined
+  });
   const $ = cheerio.load(response.data);
 
-  const resourcesDir = path.join(outputDir, 'ru-hexlet-io-courses_files');
+  const resourcesDirName = urlToDirectoryName(url);
+  const resourcesDir = path.join(outputDir, resourcesDirName);
   await fs.mkdir(resourcesDir, { recursive: true });
 
-  $('img').each(async (i, element) => {
+  const resourcePromises = $('img').map(async (_, element) => {
     const src = $(element).attr('src');
-    const resourceUrl = new URL(src, url);
-    const resourceName = urlToFileName(resourceUrl.toString());
+    if (!src) {
+      return;
+    }
+
+    const resourceUrl = new URL(src, url).toString();
+    const resourceName = urlToFileName(resourceUrl, path.extname(src));
     const resourcePath = path.join(resourcesDir, resourceName);
+    await downloadResource(resourceUrl, resourcePath);
+    $(element).attr('src', path.join(resourcesDirName, resourceName));
+  }).get();
 
-    const response = await axios.get(resourceUrl.href, { responseType: 'arraybuffer' });
-    await fs.writeFile(resourcePath, response.data);
+  await Promise.all(resourcePromises);
 
-    $(element).attr('src', path.join('ru-hexlet-io-courses_files', resourceName));
-  });
-
+  const htmlFileName = urlToFileName(url, '.html');
+  const filePath = path.join(outputDir, htmlFileName);
   const updatedHtml = $.html();
-  const filePath = path.join(outputDir, 'ru-hexlet-io-courses.html');
   await fs.writeFile(filePath, updatedHtml);
 
   return filePath;
 };
 
-const urlToFileName = (url) => {
-  const myURL = new URL(url);
-  const hostname = myURL.hostname;
-  const pathname = myURL.pathname.replace(/\/$/, '');
-  return `${hostname}${pathname}`.replace(/[^a-zA-Z0-9]/g, '-') + '.html';
-};
-
 export default downloadPage;
-
